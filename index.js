8d8e891ad140ca4ce5406f71dffc634712cbd757
Leo Crudo <crudo@scvsoft.com>
2022-04-17 10:42:12 -0300

    remove bot token from repo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Telegraf } = require("telegraf");
-const BOT_TOKEN = "5110592591:AAHQ28z7FJnPArRyL3k1FAN7H0IscRBOQTc";
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.help((ctx) => {
   ctx.reply("These are my known commands: /hello");
