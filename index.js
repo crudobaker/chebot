aaf657e79241b6fcbcbdba1791968fbdbd8f3c42
Leo Crudo <crudo@scvsoft.com>
2022-04-17 10:00:40 -0300

    initial configuration

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require("telegraf");
+const BOT_TOKEN = "5110592591:AAHQ28z7FJnPArRyL3k1FAN7H0IscRBOQTc";
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.help((ctx) => {
+  ctx.reply("These are my known commands: /hello");
+});
+
+bot.command("hello", (ctx) => ctx.reply(`Hola querido ${ctx.chat.first_name}`));
+
+bot.launch();
