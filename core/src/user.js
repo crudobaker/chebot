7a4ab5f37f62de95c955f859cc83d05df051a4ee
pablotocalini <pablotocalini@gmail.com>
2022-07-15 14:27:52 -0300

    Agregamos user test

--- core/src/user.js
+++ core/src/user.js
@@ -1,8 +1,13 @@
 export const COORDINATOR_ROLE = "coordinator";
 export const PHYSIOTHERAPIST_ROLE = "physiotherapist";
+const validRoles = [COORDINATOR_ROLE, PHYSIOTHERAPIST_ROLE];
 
 export default class User {
   constructor(id, firstName, lastName, userName, userRole) {
+    if (!validRoles.includes(userRole)) {
+      throw new Error("Invalid role");
+    }
+
     this.id = Number(id);
     this.firstName = firstName;
     this.lastName = lastName;
